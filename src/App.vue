55b319f1d857e3326635a8808cbf88c2f0a904ab
JungHoon Park <wfr112@naver.com>
2018-05-24 13:53:17 +0900

    3

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,29 @@
 <template>
   <div id="app">
-    <sub1 :exampleProp=voca />
+    <sub1 :exampleProp= voca />
+    <sub2></sub2>
   </div>
 </template>
 
 <script>
-import sub1 from "./components/sub1";
+import sub1 from "./components/sub1"
+import sub2 from "./components/sub2"
+import Vue from 'vue'
 
+const eventBus= new Vue()
+export { eventBus }
 //props로 JSON보내기 시험중
 export default {
+  eventBus,
   name: "App",
+  mounted: function() {
+    eventBus.$on('testEvent', function(value) {
+      console.log("test", value)
+    })
+  },
   components: {
-    'sub1': sub1
+    sub1,
+    sub2
   },
   data: function () {
     return {
--- src/components/sub2.vue
+++ src/components/sub2.vue
@@ -1,12 +1,19 @@
 <template>
     <div>
         sub2
+        <button v-on:click= "sendData">click</button>
     </div>
 </template>
 
 <script>
+import { eventBus } from '../App.vue'
 export default {
-
+    name: 'sub2',
+    methods: {
+        sendData: function() {
+            eventBus.$emit('testEvent', 100)
+        }
+    }
 }
 </script>
 
