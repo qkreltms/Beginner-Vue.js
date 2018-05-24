30abe7bc929ecb9d37351a35b4cfc21fe1b50f0e
JungHoon Park <wfr112@naver.com>
2018-05-24 11:25:50 +0900

    1

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <sub1 :exampleProp=voca />
+  </div>
+</template>
+
+<script>
+import sub1 from "./components/sub1";
+
+export default {
+  name: "App",
+  components: {
+    'sub1': sub1
+  },
+  data: function () {
+    return {
+      message: '',
+      voca: [
+        {"e" :"happy1", "k": "행복1"},
+        {"e" :"happy2", "k": "행복2"},
+        {"e" :"happy3", "k": "행복3"}
+      ]
+    }
+  }
+};
+</script>
---
+++ src/components/sub1.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        sub
+        <table border=1>
+            <tr v-for="item in exampleProp" :key="item.e">
+                <td>{{item.e}}</td>
+                <td>{{item.k}}</td>
+            </tr>
+        </table>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "sub1",
+    props: ['exampleProp'],
+    data() {
+      return {
+
+      }  
+    },
+    computed: {
+        test: function() {
+            return this.exampleProp
+        }
+    }
+}
+</script>
+
+<style>
+</style>
