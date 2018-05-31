6fe82100835d916de6a266024c8dcce17f0e7c7d
JungHoon Park <wfr112@naver.com>
2018-05-31 11:01:22 +0900

    6

--- src/components/sub1.vue
+++ src/components/sub1.vue
@@ -1,46 +1,37 @@
 <template>
     <div>
         sub
-<<<<<<< HEAD
         <!-- <table border=1>
             <tr v-for="item in exampleProp" :key="item.e">
                 <td>{{item.e}}</td>
-=======
-        <table border=1>
-            <tr v-for="item in voca" v-bind:key= item.e>
-                <td>v-model= item.e</td>
->>>>>>> 6f9c49afcdd1d62b185e6270a7bc5187a59179bb
                 <td>{{item.k}}</td>
             </tr>
         </table> -->
-        <ul>
-            <li v-for="item in exampleProp">{{item}}</li>
-        </ul>
+        <button v-on:click="test">test</button>
+
     </div>
 </template>
 
 <script>
 import Vue from 'vue'
-const eventBus= new Vue()
-export { eventBus }
-
 
 export default {
     name: 'sub1',
-     mounted: function() {
-    eventBus.$on('testEvent', function(value) {
-      this.voca = value
-      console.log(this.voca)
+     mounted() {
+        eventBus.$on('testEvent', function(value) {
+            console.log(this.voca1) //window에 속해있지 않기때문에 언디파인드 출력
+            this.voca1 = value
     })
   },
     data() {
       return {
-        voca: [
-            {"e" :"happy1", "k": "행복1"}
-        ]
+        voca1: "test"
       }  
     },
-    computed: {
+    methods: {
+        test: function() {
+            console.log(this.voca1)
+        }
     }
 }
 </script>
